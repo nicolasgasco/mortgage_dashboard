7ea35a778e9274571c224639fc280e051024a72a
Nicolas Gasco <nicolas.gasco@qonto.com>
2024-10-03 14:14:05 +0200

    Show progress bar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,11 @@ import {
   MORTGAGE_MONTH_DURATION,
   MORTGAGE_START_DATE,
 } from "./constants/mortgage";
-
+import {
+  CircularProgressbar,
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
 const MILLISECONDS_IN_A_MONTH = 1000 * 60 * 60 * 24 * 30.44; // Average month length in milliseconds
 const elapsedMonths =
   Math.floor(
@@ -61,12 +65,56 @@ function App() {
           onChange={onChangeRepaidAmount}
         />
       </label>
-
       <p>
         You repaid{" "}
         <strong>{Number.isNaN(repaidAmount) ? 0 : repaidAmount}</strong> months,
         which constitutes <strong>{repaidPercentage}</strong>% of your mortage.
       </p>
+      <div style={{ width: 200, height: 200 }}>
+        <CircularProgressbar
+          value={+repaidPercentage}
+          maxValue={100}
+          text={`${repaidPercentage}%`}
+          styles={{
+            // Customize the root svg element
+            root: {},
+            // Customize the path, i.e. the "completed progress"
+            path: {
+              // Path color
+              stroke: `rgba(62, 152, 199, ${repaidPercentage / 100})`,
+              // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+              strokeLinecap: "butt",
+              // Customize transition animation
+              transition: "stroke-dashoffset 0.5s ease 0s",
+              // Rotate the path
+              transform: "rotate(0.25turn)",
+              transformOrigin: "center center",
+            },
+            // Customize the circle behind the path, i.e. the "total progress"
+            trail: {
+              // Trail color
+              stroke: "#d6d6d6",
+              // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+              strokeLinecap: "butt",
+              // Rotate the trail
+              transform: "rotate(0.25turn)",
+              transformOrigin: "center center",
+            },
+            // Customize the text
+            text: {
+              // Text color
+              fill: "#f88",
+              // Text size
+              fontSize: "16px",
+            },
+            // Customize background - only used when the `background` prop is true
+            background: {
+              fill: "#3e98c7",
+            },
+          }}
+        />
+      </div>
+      ;
       <p>
         Taken into account that <strong>{elapsedMonths}</strong> months have
         elapsed since mortgage start, your mortgage is estimated to be repaid in{" "}
