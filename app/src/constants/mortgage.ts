01247e148a85517bee01f37fcdb55f403f54b787
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-10-05 23:41:34 +0200

    Change mortgage start date

--- app/src/constants/mortgage.ts
+++ app/src/constants/mortgage.ts
@@ -1,4 +1,4 @@
-export const MORTGAGE_START_DATE = new Date("2024-09-20");
+export const MORTGAGE_START_DATE = new Date("2024-10-01");
 export const MORTGAGE_AMOUNT: number = 145_000;
 export const MORTGAGE_MONTH_DURATION: number = 240;
 export const MORTGAGE_DURATION: number = 12 * 20;
