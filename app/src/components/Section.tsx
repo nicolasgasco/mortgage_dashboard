a9c73085d0d2787d367188cf2b7879bacfa2c4df
Nicolas Gasco <nicolas.gasco92@gmail.com>
2024-10-07 22:22:13 +0200

    Improve Mobile styles

--- app/src/components/Section.tsx
+++ app/src/components/Section.tsx
@@ -6,8 +6,8 @@ interface SectionProps extends PropsWithChildren {
 
 export const Section = ({ title, children }: SectionProps): JSX.Element => {
   return (
-    <div className="flex gap-3 justify-center border-slate-400 border-2 rounded-md p-4 relative my-1">
-      <h2 className="font-bold text-md absolute px-2" style={{ left: 20, top: '-5%', background: '#242424' }}>{title}</h2>
+    <div className="flex flex-col md:flex-row gap-3 justify-center border-slate-400 border-2 rounded-md p-4 relative my-1">
+      <h2 className="font-bold text-md absolute px-2" style={{ left: 20, top: -15, background: '#242424' }}>{title}</h2>
       {children}
     </div >
   )
